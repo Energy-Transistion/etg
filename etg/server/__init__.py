014d1c1811897b1891c8a751722f5e45963f7ea5
rmellema <rene.mellema@gmail.com>
2017-03-13 23:43:02 +0100

    Fixes the links in 'server/__init__.py' not working in the documentation

--- etg/server/__init__.py
+++ etg/server/__init__.py
@@ -2,16 +2,16 @@
 This module holds all of the networking code for the game. It has the following 
 submodules:
 
-#. :doc:`server`: This is the actual code for the server and it handles the 
+#. :mod:`.server`: This is the actual code for the server and it handles the 
    webhosting and creating connections.
-#. :doc:`websocket`: This contains the code for creating and handling all the
+#. :mod:`.websocket`: This contains the code for creating and handling all the
    websocket based connections.
-#. :doc:`telnet`: This contains the code for creating and handling all the 
+#. :mod:`.telnet`: This contains the code for creating and handling all the 
    telnet based connections.
-#. :doc:`company`: This contains the code for updating the clients that are
+#. :mod:`.company`: This contains the code for updating the clients that are
    companies and making sure that events from these clients are handled in the
    simulation.
-#. :doc:`party`: This contains the code for updating the clients that are
+#. :mod:`.party`: This contains the code for updating the clients that are
    parties and making sure that events from these clients are handled in the
    simulation.
 """
