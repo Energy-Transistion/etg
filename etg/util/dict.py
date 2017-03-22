7ec465e605f5fda3f6676b8de6e7734892ac8e1b
rmellema <rene.mellema@gmail.com>
2017-03-22 12:43:23 +0100

    Fixed the formatting in etg/util/dict.py

--- etg/util/dict.py
+++ etg/util/dict.py
@@ -2,15 +2,15 @@
 A set of utility functions for working with dictionaries
 """
 
-def difference(dict, other, epsilon = 0.005):
+def difference(first, other, epsilon=0.005):
     """
-    Calculate the difference between two dictionaries. The difference is 
-    defined as all the values for the keys that are in both ``dict`` and 
-    ``other`` which differ. For floating point numbers, the value ``epsilon`` 
-    is used to define difference, so two floating point values are seen as 
+    Calculate the difference between two dictionaries. The difference is
+    defined as all the values for the keys that are in both ``dict`` and
+    ``other`` which differ. For floating point numbers, the value ``epsilon``
+    is used to define difference, so two floating point values are seen as
     different if their difference is larger than ``epsilon``.
 
-    :param dict dict: The first dictionary to calculate the difference for.
+    :param dict first: The first dictionary to calculate the difference for.
     :param dict other: The other dictionary to calculate the difference for.
     :param float epsilon: The smallest difference between two floating point \
     numbers that we differentiate between
@@ -19,13 +19,13 @@ def difference(dict, other, epsilon = 0.005):
     :rtype: dict
     """
     ret = {}
-    dict_keys = set(dict.keys())
+    dict_keys = set(first.keys())
     other_keys = set(other.keys())
     for key in dict_keys.intersection(other_keys):
         try:
-            if abs(dict[key] - other[key]) >= epsilon:
+            if abs(first[key] - other[key]) >= epsilon:
                 ret[key] = other[key]
-        except:
-            if dict[key] != other[key]:
+        except TypeError:
+            if first[key] != other[key]:
                 ret[key] = other[key]
     return ret
