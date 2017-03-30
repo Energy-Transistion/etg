1223c844eec35f4c5654b4d68394ec9f905e53f4
rmellema <rene.mellema@gmail.com>
2017-03-30 15:54:38 +0200

    Made a simple class that can be used as a replacement for Netlogo's agentsets

---
+++ etg/simulation/agentset.py
@@ -0,0 +1,65 @@
+"""
+A class to mimic Netlogos agentsets iteration behaviour
+"""
+import random
+
+class AgentSet:
+    """
+    A class to mimic Netlogo's agentsets random iteration behaviour. Also has
+    some convience methods that making common sequence operations for which
+    order does not matter faster.
+    """
+    def __init__(self, *args, _list=None, _iter=None):
+        """
+        An object can be initialized by giving the agents that shoudl initially
+        be in the set in its constructor.
+        """
+        if _list:
+            self._list = _list
+        elif _iter:
+            self._list = list(_iter)
+        else:
+            self._list = list(args)
+
+    def __iter__(self):
+        """
+        Return an iterator that goes over the agents in a random order.
+        """
+        random.shuffle(self._list)
+        return iter(self._list)
+
+    def __len__(self):
+        """
+        Get the number of agents in this agentset.
+        """
+        return len(self._list)
+
+    def __contains__(self, agent):
+        """
+        A check to see if a certain agent is in the set.
+        """
+        return agent in self._list
+
+    def add_agent(self, agent):
+        """
+        Add an agent to the agent set. If the agent is already in the set, do
+        nothing.
+        """
+        if agent not in self._list:
+            self._list.append(agent)
+
+    def remove_agent(self, agent):
+        """
+        Remove an agent from the set. If the agent is not in the set, do
+        nothing.
+        """
+        self._list.remove(agent)
+
+    def filter(self, predicate):
+        """
+        A more efficient filter that works directly on the underlaying
+        datastructure, and therefore does not shuffle the list first. If order
+        is not important for the filtering, this method is preferred over the
+        built in filter
+        """
+        return AgentSet(_iter=filter(predicate, self._list))
---
+++ tests/test_etg/test_simulation/test_agentset.py
@@ -0,0 +1,58 @@
+"""
+A testing suite for the `etg.simulation.agentset` module
+"""
+from twisted.trial import unittest
+from etg.simulation.agentset import AgentSet
+
+class AgentSetTestCase(unittest.TestCase):
+    """
+    Testing the behaviours for the agent set.
+    """
+
+    def test_len(self):
+        """
+        A test to make sure that `len` works properly.
+        """
+        self.assertEqual(len(AgentSet(1, 2, 3, 4, 5)), 5)
+        self.assertEqual(len(AgentSet([1, 2, 3, 4, 5])), 1)
+        self.assertEqual(len(AgentSet(_list=[1, 2, 3, 4, 5])), 5)
+        self.assertEqual(len(AgentSet(_iter=set([1, 2, 3, 4, 5]))), 5)
+
+    def test_contains(self):
+        """
+        A test to make sure that `in` works properly.
+        """
+        agents = AgentSet(1, 2, 3, 4, 5)
+        self.assertTrue(3 in agents)
+        self.assertFalse(7 in agents)
+        self.assertFalse(3 not in agents)
+        self.assertTrue(7 not in agents)
+
+    def test_add(self):
+        """
+        A test to make sure that agents get added to the set.
+        """
+        agents = AgentSet(_iter=range(10))
+        agents.add_agent(20)
+        self.assertTrue(20 in agents)
+
+    def test_remove(self):
+        """
+        A test to make sure that agents get properly removed.
+        """
+        agents = AgentSet(_iter=range(10))
+        agents.remove_agent(5)
+        self.assertTrue(5 not in agents)
+
+    def test_iter(self):
+        """
+        A test to make sure that the agents get properly shuffled. Uses
+        randomness, so might occasionally fail.
+        """
+        agents = AgentSet(_iter=range(100))
+        sets = []
+        for _ in range(5):
+            permutation = list(agents)
+            self.assertTrue(permutation not in sets,
+                            "There are two agent sets with the same order! Rerun to make sure")
+            sets.append(permutation)
