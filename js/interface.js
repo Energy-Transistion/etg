a4dc0010f47db367fba5f947a76df75674f5f5c1
rmellema <rene.mellema@gmail.com>
2017-05-04 01:34:11 +0200

    Added the templates and styles for the base interface

---
+++ js/interface.js
@@ -0,0 +1,103 @@
+/* Vue components */
+Vue.component('etg-monitor', {
+  template: '<div class="monitor"><h4><slot>{{ monitor }}</slot></h4><span>{{ value }}</span></div>',
+  props: {
+    monitor: {
+      type: String,
+      required: true
+    }
+  },
+  data: function() {
+    return {
+      value: 0
+    }
+  }
+})
+
+Vue.component('plot', {
+  template: '<div class = "plot"></div>',
+})
+
+/* Tab functionality */
+function openTab(event, name) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the link that opened the tab
+  document.getElementById(name).style.display = "block";
+  event.currentTarget.className += " active";
+}
+
+
+/* Setting up everything */
+window.onload = function() {
+  window.vm = new Vue({
+    el: '#content',
+    delimiters: ['[[', ']]'],
+    data: {
+      date: 'Today',
+      weather: 'Quite Sunny',
+      newsfeed: [
+        'Nuclear reactor exploded',
+        'More bullshit news',
+        'Clown elected president on Mars',
+        'djkajflkdasfdalkjfdkl',
+        'A puppy got lost',
+        'We have no more icecream',
+        'The moon exploded',
+        'The summer is actually not coming',
+      ],
+      solar: {
+        price_bruto: 0,
+        price_average: 0,
+        tax: 0,
+        level: 0,
+        upgrade_price: 10000,
+        sell_price: 5000,
+      },
+      wind: {
+        price_bruto: 0,
+        price_average: 0,
+        tax: 0,
+        upgrade_price: 10000,
+        sell_price: 5000,
+      },
+      nuclear: {
+        price_bruto: 0,
+        price_average: 0,
+        tax: 0,
+        upgrade_price: 10000,
+        sell_price: 5000,
+      },
+      oil: {
+        price_bruto: 0,
+        price_average: 0,
+        tax: 0,
+        upgrade_price: 10000,
+        sell_price: 5000,
+      },
+      gas: {
+        price_bruto: 0,
+        price_average: 0,
+        tax: 0,
+        upgrade_price: 10000,
+        sell_price: 5000,
+      },
+      coal: {
+        price_bruto: 0,
+        price_average: 0,
+        tax: 0,
+        upgrade_price: 10000,
+        sell_price: 5000,
+      },
+    }
+  })
+  openTab({currentTarget: document.getElementById('button-Inputs')}, 'Inputs');
+}
