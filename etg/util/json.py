d28fc206fc7abad9e0495abf3f629edff7d24d63
rmellema <rene.mellema@gmail.com>
2017-05-14 22:25:45 +0200

    Removed cyclical reference in the JSON serializer

--- etg/util/json.py
+++ etg/util/json.py
@@ -5,6 +5,7 @@ import copy
 import datetime
 import json
 from etg.simulation.energy import EnergyType, Producer
+from etg.simulation.entity import Entity
 
 def default(obj):
     """
@@ -12,9 +13,13 @@ def default(obj):
     """
     if isinstance(obj, datetime.date):
         return obj.isoformat()
-    elif isinstance(obj, EnergyType):
-        _dict = copy.copy(obj.__dict__)
-        del _dict['simulation']
+    elif isinstance(obj, EnergyType) or isinstance(obj, Entity):
+        _dict = {}
+        for key, value in obj.__dict__.items():
+            if key == "simulation":
+                pass
+            else:
+                _dict[key] = value
         return _dict
     elif isinstance(obj, Producer):
         return obj.__dict__
