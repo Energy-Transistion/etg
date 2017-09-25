654f6ab8da6dbe0e6fe811cf41347ad63ee9e0b7
rmellema <rene.mellema@gmail.com>
2017-09-25 14:57:29 +0200

    Made the instal script more robust

--- bin/install.py
+++ bin/install.py
@@ -3,6 +3,7 @@
 
 from os import path
 from subprocess import call
+from sys import executable
 
 print("Installing the ETG.")
 real_path = path.realpath(__file__)
@@ -12,11 +13,18 @@ print("Installing into " + main_dir)
 venv = path.join(main_dir, 'venv')
 print("Putting dependencies into " + venv)
 print("Setting up the virtual environment")
-call('python3 -m venv ' + venv, shell=True)
-pip = path.join(venv, 'bin', 'pip')
+call(executable + ' -m venv "' + venv + '"', shell=True)
+if path.exists(path.join(venv, 'bin')):
+    bin_dir = 'bin'
+else:
+    bin_dir = 'Scripts'
+pip = path.join(venv, bin_dir, 'pip')
 print(pip)
+# Cause Windows is retarded or something
+if not path.exists(pip):
+    call('"' + path.join(venv, bin_dir, 'python') + '" -m ensurepip --default-pip', shell=True)
 requirements = path.join(main_dir, 'requirements', 'base.txt')
 print(requirements)
 print("Installing the requirements")
-call(pip + ' install -r ' + requirements, shell=True)
+call('"' + pip + '" install -r "' + requirements + '"', shell=True)
 print("Done!")
